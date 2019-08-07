7984a3adec8dcffb01b65aaf70cb620deb96eb0e
Doomer77 <newzibkoff1984@gmail.com>
2019-08-07 19:56:39 +0300

    add slider

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: true,
+    fade: true, 
+    autoplaySpeed: 3000,
+    speed: 2000,
+    pauseOnHover: false,
+    dots: true 
+  }); 
+});
